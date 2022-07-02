ba38f5e33b8d79d405f4c57527d017e2bcf4ae65
VICTOR <VICTOR@CHEN-PUNAK-DESK>
2022-07-03 01:08:56 +0700

    Add project files.

---
+++ Simple RFID Reader with Aduino UNO/FrmMainMenu.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Simple_RFID_Reader_with_Aduino_UNO
+{
+    partial class FrmMainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 462);
+            this.Name = "frmMainMenu";
+            this.Text = "RFID Manager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
